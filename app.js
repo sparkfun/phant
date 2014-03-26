751dedb28c98ce9745b807cfb56bcf5ab830b9b3
Todd Treece <toddtreece@gmail.com>
2014-03-26 11:42:15 -0400

    adds cli for stream creation

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var Phant = require('./lib/phant'),
     dotenv = require('dotenv').load(),
+    path = require('path'),
     httpServer = require('phant-http-server'),
     HttpInput = require('phant-input-http'),
-    HttpManager = require('phant-manager-http'),
     HttpOutput = require('phant-output-http'),
     keychain = require('phant-keychain-hex'),
-    storage = require('phant-storage-mongodb'),
+    storage = require('phant-storage-json'),
     app = Phant();
 
 var keys = keychain({
@@ -13,28 +13,22 @@ var keys = keychain({
   privateSalt: process.env.PRIVATE_SALT || 'private salt'
 });
 
-var mongo = storage({
-  url: process.env.MONGO_URL || 'mongodb://localhost/phant',
-  cap: process.env.CAP || false
+var json = storage({
+  directory: path.join(__dirname, 'tmp')
 });
 
 var httpOutput = HttpOutput({
-  storage: mongo,
+  storage: json,
   keychain: keys
 });
 
 var httpInput = HttpInput({
-  metadata: mongo,
-  keychain: keys
-});
-
-var httpManager = HttpManager({
-  storage: mongo,
+  metadata: json,
   keychain: keys
 });
 
 // start listening for connections
-httpServer.listen(process.env.PORT || 5000);
+httpServer.listen(process.env.PORT || 8080);
 
 // attach input to http server
 httpServer.use(httpInput);
@@ -45,13 +39,7 @@ httpServer.use(httpOutput);
 // register input with phant
 app.registerInput(httpInput);
 
-// attach input to http server
-httpServer.use(httpManager);
-
-// register manager with phant
-app.registerManager(httpManager);
-
-app.registerOutput(mongo);
+app.registerOutput(json);
 app.registerOutput(httpOutput);
 
 exports = module.exports = app;
