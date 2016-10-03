2651edde808a29f7ded14ad875f17ad0eeeb24e5
Stephen Hawkes <steve@blanc.ltd.uk>
2016-10-03 23:01:10 +0100

    Update http_post.py to Python 3
    It's hacktober and wanted to show Sparkfun some love. Spotted this... thought it might be useful for the future.
    
    Should be PEP8 compliant, I broke out the 'code' parameter to hint to users about building dictionaries.

--- examples/python/http_post.py
+++ examples/python/http_post.py
@@ -1,39 +1,36 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-""" Simple http POST example using Python 2.7 and urllib and urllib2."""
+"""Simple HTTP POST example using Python 3 and requests."""
 
-import urllib
-import urllib2
+import requests
+import sys
 
-public_hash     = 'KJJGv81n1pUyM4Kbg9by'
-private_hash    = 'vzzjDZR6RpfjdEzb1Ybj'
-base_url        = 'https://data.sparkfun.com'
-post_url        = base_url + '/input/' + public_hash
 
+public_hash = 'KJJGv81n1pUyM4Kbg9by'
+private_hash = 'vzzjDZR6RpfjdEzb1Ybj'
+url = ''.join(('https://data.sparkfun.com/input/', public_hash))
 
 headers = {
-	'Content-type': 'application/x-www-form-urlencoded',
-	'Phant-Private-Key': private_hash
+    'Content-type': 'application/x-www-form-urlencoded',
+    'Phant-Private-Key': private_hash
 }
 
 
 def main():
+    data = {
+        'who': input('Who are you? '),
+        'where': input('Where are you? '),
+        'favorite_animal': input("What's your favorite animal? "),
+    }
+    data['code'] = 'python'
 
-	data = {}
-	data['who'] = raw_input("Who are you? ")
-	data['where'] = raw_input("Where are you? ")
-	data['favorite_animal'] = raw_input("What's your favorite animal? ")
-	data['code'] = 'python'
+    try:
+        r = requests.post(url, data=data, headers=headers)
+        print(r.text)
 
-	data = urllib.urlencode(data)
-	post_request = urllib2.Request(post_url,data,headers)
-
-	try: 
-		post_response = urllib2.urlopen(post_request)
-		print post_response.read()
-
-	except URLError as e:
-		print "Error: ", e.reason
+    except requests.exceptions.RequestException as e:
+        print(e)
+        sys.exit(1)
 
 if __name__ == "__main__":
-	main()
+    main()
